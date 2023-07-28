8554c2d0ac937e368307ad2273c68c4a35298daf
XLor <yjl9903@vip.qq.com>
2023-07-28 23:16:05 +0800

    refresh: 2023/7/28 23:16:05

---
+++ commit.ts
@@ -0,0 +1,10 @@
+#!/usr/bin/env optc
+
+/// <reference types="optc/globals" />
+
+export default async function() {
+  const date = new Date();
+  const commit = `refresh: ${date.toLocaleString()}`;
+  await $`git add .`;
+  await $`git commit -m ${commit}`;
+}
