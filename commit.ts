d021bbbd5fb8cfac4fe983bd5b2167f9a4afa8cd
XLor <yjl9903@vip.qq.com>
2023-07-28 23:16:48 +0800

    feat: add commit script

--- commit.ts
+++ commit.ts
@@ -5,6 +5,8 @@
 export default async function() {
   const date = new Date();
   const commit = `refresh: ${date.toLocaleString()}`;
+
   await $`git add .`;
   await $`git commit -m ${commit}`;
+  await $`git push origin main`;
 }
